9a391799918c04de14f041c3e8a5b985a3a427eb
Esteban Sánchez <e.sanchez@globant.com>
2020-12-02 10:31:07 -0600

    hotfixes

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 
-// import YoutubePlayer from '../components/YoutubePlayer';
 import Player from '../components/Player'
-import {CastProvider} from '../contexts/cast.context';
+import { CastProvider } from '../contexts/cast.context';
 import Splash from '../components/Splash';
-// import { YoutubeProvider } from '../contexts/youtube.context';
 
 function App() {
   const [playerReady, setPlayerReady] = useState(false);
+
   return (
     <CastProvider>
       <Splash playerReady={playerReady} />
